2564d671e244ac9e439fbe28c48b84838da73e7e
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-05 13:53:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int srednee;
+    int leftarea = 0;
+    int rightarea = size - 1;
+    int index = -2;
+    while ((leftarea <= rightarea)) {
+        srednee = (leftarea + rightarea) / 2;
+        if (arr[srednee] == value) {
+            index = srednee;
+        }
+        if (arr[srednee] < value) {
+            leftarea = srednee + 1;
+        }
+        else {
+            rightarea = srednee - 1;
+        }
+    }
+    if (index != -2) {
+        int point = index;
+        int count = 0;
+        while (arr[index] == value) {
+            count++;
+            index-= 1;
+        }
+        index = point + 1;
+        while (arr[index] == value) {
+            count++;
+            index+= 1;
+        }
+        return count;
+    }
+    return 0;
 }
